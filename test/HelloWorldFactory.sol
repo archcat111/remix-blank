cf342a3563b9f24a02b04b67ca5e2eaf5d44993f
archcat111 <139642011@qq.com>
2025-02-06 19:32:13 +0800

    test

--- test/HelloWorldFactory.sol
+++ test/HelloWorldFactory.sol
@@ -1,25 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// 方法1：直接将HelloWorld.sol中的HelloWorld合约代码复制过来，一个sol文件中可以有多个contract
-
-// 方法2：引入HelloWorld.sol文件，这里HelloWorld.sol在同目录下，这种方法会将HelloWorld.sol文件里的所有合约都引入进来
-// import "./HelloWorld.sol"; 
-
-// 方法3：只引入HelloWorld.sol文件中的HelloWorld合约
- import {HelloWorld} from "./HelloWorld.sol"; 
-
-// 方法4：引入网络上的合约
-// import {HelloWorld} from "https://github.com/archcat111/remix-blank/blob/main/test/HelloWorld.sol"; 
-
-// 方法5：引入通过NPM安装的包中的合约
-// import {HelloWorld} from "@companyName/product/contract"
+import {HelloWorld} from "./HelloWorld.sol"; 
 
 contract HelloWorldFactory {
 
-    HelloWorld helloWorld;
+    HelloWorld[] helloWorlds;
 
     function createHelloWorld() public {
-        helloWorld = new HelloWorld();
+        helloWorlds.push(new HelloWorld());
+    }
+
+    function getHelloWorldByIndex(uint256 _index) public view returns(HelloWorld){
+        return helloWorlds[_index];
+    }
+
+    function callSayHelloFromFactory(uint256 _index, uint256 _id) public view returns(string memory) {
+        return helloWorlds[_index].sayHello(_id);
+    }
+
+    function callSetHelloWorldFromFactory(uint256 _index, string memory _newString, uint256 _id) public {
+        helloWorlds[_index].setHelloWorld(_newString, _id);
     }
 }
