506c97a1d9b84f61e3e1c92f67474df1b97981b6
archcat111 <139642011@qq.com>
2025-02-06 18:59:01 +0800

    test

---
+++ test/HelloWorldFactory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// 方法1：直接将HelloWorld.sol中的HelloWorld合约代码复制过来，一个sol文件中可以有多个contract
+
+// 方法2：引入HelloWorld.sol文件，这里HelloWorld.sol在同目录下，这种方法会将HelloWorld.sol文件里的所有合约都引入进来
+// import "./HelloWorld.sol"; 
+
+// 方法3：只引入HelloWorld.sol文件中的HelloWorld合约
+import {HelloWorld} from "./HelloWorld.sol"; 
+
+// 方法4：引入网络上的合约
+
+contract HelloWorldFactory {
+
+    HelloWorld helloWorld;
+
+    function createHelloWorld() public {
+
+    }
+}
